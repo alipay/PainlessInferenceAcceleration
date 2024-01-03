db45c690ab3f7eac44d120c01af8a0315c230d8b
nanxiao.zy <nanxiao.zy@antgroup.com>
2024-01-03 17:11:33 +0800

    remove install_requires in setup.py

--- pia/lookahead/datasets/dataset.py
+++ pia/lookahead/datasets/dataset.py
@@ -3,6 +3,6 @@
 
 """
 the datasets used for benchmarks can be found in huggingface.co, i.e.,:
-dolly_15k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/dolly_15k_llama2_7b_chat/viewer/default/train?p=1
-gsm_8k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/gsm_8k_llama2_7b_chat/viewer/default/train?p=1
+dolly_15k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/dolly_15k_llama2_7b_chat/tree/main
+gsm_8k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/gsm_8k_llama2_7b_chat/tree/main
 """
