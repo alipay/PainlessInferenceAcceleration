72324f752d42117650be6c16713a20411d49459e
nanxiao.zy <nanxiao.zy@antgroup.com>
2024-01-22 13:55:19 +0800

    add known issues

--- pia/lookahead/examples/qwen_quant_example.py
+++ pia/lookahead/examples/qwen_quant_example.py
@@ -19,6 +19,7 @@ from pia.lookahead.examples import local_path_dict
 
 model_dir = local_path_dict.get('qwen_quant', 'your/model/path') 
 
+# should install auto_gptq at first
 model = QWenLMHeadModel.from_pretrained(model_dir,
                                         device_map="auto",
                                         trust_remote_code=True
@@ -33,7 +34,7 @@ prompt = "编一个200字左右的儿童故事"
 
 for use_lookahead in [False, False, True, True]:
     debug_lookahead = False
-    decoding_length = 128
+    decoding_length = 64
     branch_length = 12
     max_new_tokens = 256
     decoding_kwargs = {"use_lookahead": use_lookahead,
@@ -43,7 +44,7 @@ for use_lookahead in [False, False, True, True]:
                        "branch_length": branch_length,
                        "stop_words": stop_words}
     model.generation_config.decoding_kwargs=decoding_kwargs
-    model.generation_config.do_sample=False
+    model.generation_config.do_sample=False  # default is True for qwen, result in different responses in every generation
     model.generation_config.repetition_penalty=None  # repetition_penalty is not fully supported currently, will fix in the future
     ts = time.time()
     response, history = model.chat(tokenizer, prompt, history=None, eos_token_id=151645)
