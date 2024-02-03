9a3a1a131f1935ee70c798cccc21ce14114ca561
nanxiao.zy <nanxiao.zy@antgroup.com>
2024-02-03 20:08:13 +0800

    opt benchmark

--- pia/lookahead/benchmarks/glm_benchmark.py
+++ pia/lookahead/benchmarks/glm_benchmark.py
@@ -18,20 +18,17 @@ class GlmBenchmark(Benchmark):
         from pia.lookahead.models.glm.modeling_glm import GLMForConditionalGeneration
         from pia.lookahead.models.glm.tokenization_glm import GLMChineseTokenizer
         model = GLMForConditionalGeneration.from_pretrained(model_dir
-                                                            , cache_dir='../'
-                                                            , offload_folder='./'
                                                             , torch_dtype=torch.float16
                                                             , low_cpu_mem_usage=True
-                                                            , device_map='auto')
+                                                            , device_map={"":"cuda:0"})
         tokenizer = GLMChineseTokenizer.from_pretrained(model_dir)
         tokenizer.pad_token = tokenizer.eos_token
-
-        stop_ids = tokenizer.convert_tokens_to_ids(self.stop_words+['的','是'])
-        model.lookahead_cache = LookaheadCache(stop_words=stop_ids)
+        model.lookahead_cache = LookaheadCache()
         self.model = model
         self.tokenizer = tokenizer
         self.eos = tokenizer.eop_token_id
         self.eop = 50006
+        self.stop_ids = tokenizer.convert_tokens_to_ids(self.stop_words+['的','是'])
 
     def tokenize(self, prompt, max_length=256):
         tokenizer = self.tokenizer
@@ -57,26 +54,25 @@ class GlmBenchmark(Benchmark):
         return input_ids, position_ids, attention_mask
 
 
-model_dir = 'your/model/path'
-"""
-generate answers first if only prompts are available, answers in the warmup samples are used for constructing trie-tree cache
-prompt_dir = 'your/prompt/dir'
-dataset_dir = 'your/answer/dir'
-worker.save_answers(prompt_dir, answer_dir, prompt_name='your/prompt/field/name', batch_size=1, max_count=None)
-"""
-dataset_dir = 'your/dataset/path'
-
+model_dir = '/mntnlp/nanxiao/lookahead/antglm'
 worker = GlmBenchmark(log_dir='antglm_benchmark')
 worker.initialize(model_dir=model_dir, token_dir=model_dir)
-worker.load_prompts(prompt_dir=dataset_dir)
 
-max_length = 256
-chat_count = 1000
-warmup_count = 10000
+"""
+answers in the warmup samples are used for constructing trie-tree cache
+"""
+# warmup_prompt_dir = '/mntnlp/nanxiao/dataset/antrag_8k/train.jsonl'
+# warmup_dataset_dir = '/mntnlp/nanxiao/dataset/lookahead/antrag_8k_antglm_10b/train.jsonl'
+# worker.save_answers(warmup_prompt_dir, warmup_dataset_dir, batch_size=1, max_count=None, use_lookahead=True)
+
+# dataset_dir = 'your/dataset/path'
+dataset_dir = '/mntnlp/nanxiao/dataset/lookahead/antrag_8k_antglm_10b/test.jsonl'
+warmup_dataset_dir = '/mntnlp/nanxiao/dataset/lookahead/antrag_8k_antglm_10b/train.jsonl'
+worker.load_prompts(prompt_dir=dataset_dir, warmup_prompt_dir=warmup_dataset_dir)
 
 # test correctness with lookahead decoding
 worker.batch_chat(worker.prompts[:10],
-                  max_length=max_length,
+                  max_new_tokens=256,
                   decoding_length=16,
                   branch_length=4,
                   debug_lookahead=False,
@@ -84,7 +80,10 @@ worker.batch_chat(worker.prompts[:10],
                   batch_size=1)
 
 # performance check
-worker.perf_check(worker.prompts[:chat_count], warmup_ids=worker.ids[chat_count:chat_count + warmup_count],
-                  sizes=[64], lens=[0], max_length=max_length)
-worker.perf_check(worker.prompts[:chat_count], warmup_ids=worker.ids[chat_count:chat_count + warmup_count],
-                  sizes=[64], lens=[12], max_length=max_length)
+max_new_tokens = 256
+chat_count = 1000
+warmup_count = 10000
+worker.perf_check(worker.prompts[:chat_count], warmup_ids=worker.warmup_ids[:warmup_count],
+                  sizes=[0], lens=[0], max_new_tokens=max_new_tokens)
+worker.perf_check(worker.prompts[:chat_count], warmup_ids=worker.warmup_ids[:warmup_count],
+                  sizes=[64,128], lens=[24,32], max_new_tokens=max_new_tokens,decoding_mode='one_input')
