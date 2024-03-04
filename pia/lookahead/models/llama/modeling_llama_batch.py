3b22989923f2be42735fe3dd1ba7f098e2c5c104
nanxiao.zy <nanxiao.zy@antgroup.com>
2024-03-04 18:50:24 +0800

    adapt transformers verion

--- pia/lookahead/models/llama/modeling_llama_batch.py
+++ pia/lookahead/models/llama/modeling_llama_batch.py
@@ -76,22 +76,6 @@ def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] 
     return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
 
 
-# class LlamaRMSNorm(nn.Module):
-#     def __init__(self, hidden_size, eps=1e-6):
-#         """
-#         LlamaRMSNorm is equivalent to T5LayerNorm
-#         """
-#         super().__init__()
-#         self.weight = nn.Parameter(torch.ones(hidden_size))
-#         self.variance_epsilon = eps
-
-#     def forward(self, hidden_states):
-#         input_dtype = hidden_states.dtype
-#         variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
-#         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-
-#         return (self.weight * hidden_states).to(input_dtype)
-
 class LlamaRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         """
@@ -102,7 +86,23 @@ class LlamaRMSNorm(nn.Module):
         self.variance_epsilon = eps
 
     def forward(self, hidden_states):
-        return rmsnorm_wrapper(hidden_states, self.weight, eps=self.variance_epsilon)
+        input_dtype = hidden_states.dtype
+        variance = hidden_states.to(torch.float32).pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+
+        return (self.weight * hidden_states).to(input_dtype)
+
+# class LlamaRMSNorm(nn.Module):
+#     def __init__(self, hidden_size, eps=1e-6):
+#         """
+#         LlamaRMSNorm is equivalent to T5LayerNorm
+#         """
+#         super().__init__()
+#         self.weight = nn.Parameter(torch.ones(hidden_size))
+#         self.variance_epsilon = eps
+
+#     def forward(self, hidden_states):
+#         return rmsnorm_wrapper(hidden_states, self.weight, eps=self.variance_epsilon)
 
 
 class LlamaRotaryEmbedding(torch.nn.Module):
