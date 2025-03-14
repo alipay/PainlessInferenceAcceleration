2ee29953183964e12954ca632e70872036eba77b
nanxiao.zy <nanxiao.zy@antgroup.com>
2025-03-14 15:12:43 +0800

    adapt for sm86 & fix func name

--- flood/csrc/activation/activation_kernels.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Ant Financial Service Group and its affiliates.
-
-#include <torch/extension.h>
-
-void silu_and_mul(torch::Tensor& out, torch::Tensor& input);
--- lookahead/datasets/dataset.py
+++
@@ -1,8 +0,0 @@
-
-
-
-"""
-the datasets used for benchmarks can be found in huggingface.co, i.e.,:
-dolly_15k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/dolly_15k_llama2_7b_chat/tree/main
-gsm_8k_llama2_7b_chat.jsonl : https://huggingface.co/datasets/zheyishine/gsm_8k_llama2_7b_chat/tree/main
-"""
