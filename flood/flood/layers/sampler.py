fix lookahead bug

flood/flood/layers/sampler.py
@@ -30,15 +30,21 @@ class Sampler(torch.nn.Module):
         # lookahead
         if batch_meta_info.mode == 2:
             next_ids = logits.argmax(dim=-1)
-            output_ids, cache_src_indices, cache_dst_indices = batch_meta_info.spec.verify_draft(batch_meta_info.input_ids, 
-                                              next_ids,
-                                              batch_meta_info=batch_meta_info)
+            output_ids, cache_src_indices, cache_dst_indices = \
+                batch_meta_info.spec.verify_draft(batch_meta_info.input_ids, 
+                                                  next_ids,
+                                                  batch_meta_info=batch_meta_info)
             output_ids = [[y for y in x if y!=-1] for x in output_ids.tolist()]
-            # accept_counts = [len(x) for x in output_ids]
-            # print(f'{accept_counts=}')
+            
+            if False:
+                accept_counts = [len(x) for x in output_ids]
+                texts = batch_meta_info.spec.tokenizer.batch_decode(output_ids)
+                print(f'{accept_counts=} {output_ids=} {texts=}')
+                if tuple(output_ids[0]) == tuple([4891, 236, 228, 99497]):
+                    print(f'{cache_src_indices=} {cache_dst_indices=}')
+
             for i, req in enumerate(reqs):
                 req.output_ids.extend(output_ids[i])
-            # print(f'{output_ids=} {cache_src_indices=} {cache_dst_indices=}')
             batch_meta_info.cache_src_indices = cache_src_indices
             batch_meta_info.cache_dst_indices = cache_dst_indices
             return 

flood/flood/ops/draft.py
@@ -17,6 +17,7 @@ def update_draft_table_kernel(
         draft_table,
         token_count,
         stride,
+        vocab,
         BRANCH_LENGTH: tl.constexpr,
         BRANCH_COUNT: tl.constexpr,
         BLOCK: tl.constexpr,
@@ -28,8 +29,8 @@ def update_draft_table_kernel(
         if bid * BLOCK + i + 4 <= token_count:
             p0 = tl.load(tokens + bid * BLOCK + i, mask = bid * BLOCK + i < token_count, other=0)
             p1 = tl.load(tokens + bid * BLOCK + i + 1, mask = bid * BLOCK + i + 1 < token_count, other=0)
-            uid = p0 * 8191 + p1
-            bucket_idx = uid % SIZE
+            uid = p0.to(tl.int64) * vocab + p1
+            bucket_idx = uid % (SIZE - BRANCH_COUNT)
             branch = tl.load(tokens + bid * BLOCK + 2 + i + indices, 
                              mask=bid * BLOCK + 2 + i + indices < token_count, 
                              other=0)
@@ -88,7 +89,7 @@ def update_draft_table(tokens,
                        table_size=2 ** 16, 
                        branch_length=8,
                        branch_count=8, 
-                       vocab=100000, 
+                       vocab=128256, 
                        eos=0):
     # tokens: list of token_id, 
     # table: [sum(sizes),(1+length)], table_meta: [32(frq),32*length]
@@ -107,6 +108,7 @@ def update_draft_table(tokens,
         draft_table,
         token_count,
         draft_table.stride(0),
+        vocab,
         BRANCH_LENGTH=branch_length,
         BRANCH_COUNT=branch_count,
         BLOCK=block,
@@ -123,6 +125,7 @@ def retrieve_draft_table_kernel(
         freq_table,
         draft_table,
         stride,
+        vocab,
         BRANCH_COUNT: tl.constexpr,
         BRANCH_LENGTH: tl.constexpr,
         RETRIEVE_COUNT: tl.constexpr,
@@ -133,7 +136,7 @@ def retrieve_draft_table_kernel(
 
     p0 = tl.load(tokens + bid * 2)
     p1 = tl.load(tokens + bid * 2 + 1)
-    uid = p0 * 8191 + p1
+    uid = p0.to(tl.int64) * vocab + p1
     bucket_idx = uid % (SIZE - BRANCH_COUNT)
 
     hit = False
@@ -181,6 +184,7 @@ def retrieve_draft_table(tokens,
                          freq_table, 
                          draft_table, 
                          table_size=2 ** 16,
+                         vocab=128256,
                          branch_length=8, 
                          branch_count=8, 
                          retrieve_count=8):
@@ -202,13 +206,14 @@ def retrieve_draft_table(tokens,
         torch.ones((batch_size, l, l), 
                     device=device, 
                     dtype=torch.int8),
-        diagonal=1)
-    for i in range(batch_size):
-        for j in range(1, retrieve_count):
-            output_masks[i,j * branch_length + 1:(j + 1) * branch_length + 1,
+        diagonal=0)
+    for j in range(1, retrieve_count):
+        output_masks[:,j * branch_length + 1:(j + 1) * branch_length + 1,
             1:j * branch_length + 1] = 0
-    output_masks = output_masks.view(batch_size * l, l)
+    # print(output_masks.cpu().numpy()[0,:8,:8])
+    # print(output_masks.cpu().numpy()[0,-8:,-8:])
 
+    # output_masks = output_masks.view(batch_size * l, l)
     grid = lambda META: (batch_size,)
     retrieve_draft_table_kernel[grid](
         output_tokens,
@@ -216,6 +221,7 @@ def retrieve_draft_table(tokens,
         freq_table,
         draft_table,
         draft_table.stride(0),
+        vocab,
         BRANCH_COUNT=branch_count,
         BRANCH_LENGTH=branch_length,
         RETRIEVE_COUNT=retrieve_count,
@@ -304,10 +310,10 @@ def verify_draft(input_ids, next_ids, cache_offsets, masks, batch_size,
     output_ids = -1 * torch.ones((batch_size, branch_length + 1),
                                  device=device, 
                                  dtype=input_ids.dtype)
-    cache_src_indices = -1*torch.ones((batch_size, branch_length),
+    cache_src_indices = -1*torch.ones((batch_size*branch_length,),
                                      device=device,
                                      dtype=input_ids.dtype)
-    cache_dst_indices = -1*torch.ones((batch_size, branch_length),
+    cache_dst_indices = -1*torch.ones((batch_size*branch_length,),
                                      device=device,
                                      dtype=input_ids.dtype)
     batch_input_ids = torch.reshape(input_ids, (batch_size, branch_count, branch_length))
@@ -350,47 +356,35 @@ def verify_draft(input_ids, next_ids, cache_offsets, masks, batch_size,
 
 
 @triton.jit
-def update_draft_cache_kernel(k_cache,
-                              v_cache,
+def update_draft_cache_kernel(cache,
                               src_indices,
                               dst_indices,
                               dim,
-                              token_count,
                               DIM: tl.constexpr
-                              ):
-    bid = tl.program_id(0)
-    sm = tl.num_programs(0)
-    token_per_sm = (token_count - 1) // sm + 1
+                            ):
+    pid = tl.program_id(0)
     indices = tl.arange(0, DIM)
-    for i in range(token_per_sm):
-        if i + token_per_sm * bid < token_count:
-            src_idx = tl.load(src_indices + token_per_sm * bid + i)
-            dst_idx = tl.load(dst_indices + token_per_sm * bid + i)
-            if src_idx >= 0:
-                if dst_idx >= 0:
-                    k_vals = tl.load(k_cache + src_idx * dim + indices,
-                                    mask=indices < dim, other=0.0)
-                    tl.store(k_cache + dst_idx * dim + indices, k_vals,
-                            mask=indices < dim)
-                    v_vals = tl.load(v_cache + src_idx * dim + indices,
-                                    mask=indices < dim, other=0.0)
-                    tl.store(v_cache + dst_idx * dim + indices, v_vals,
-                            mask=indices < dim)
+
+    src_idx = tl.load(src_indices + pid)
+    
+    if src_idx >= 0:
+        dst_idx = tl.load(dst_indices + pid)
+        vals = tl.load(cache + src_idx * dim + indices,
+                        mask=indices < dim, other=0.0)
+        tl.store(cache + dst_idx * dim + indices, vals,
+                mask=indices < dim)
 
 
-def update_draft_cache(k_cache, v_cache, src_indices, dst_indices, sm=78):
-    num_head, head_dim = k_cache.size(1), k_cache.size(2)
+def update_draft_cache(cache, src_indices, dst_indices):
+    dim = cache.size(1)
     token_count = src_indices.size(0)
-    dim = num_head * head_dim
     round_dim = 2 ** (int(math.log2(dim - 1) + 1))
 
-    grid = lambda META: (sm,)
-    update_draft_cache_kernel[grid](k_cache, 
-                              v_cache,
+    grid = lambda META: (token_count,)
+    update_draft_cache_kernel[grid](cache, 
                               src_indices, 
                               dst_indices,
                               dim,
-                              token_count,
                               DIM=round_dim,
                               num_warps=4,
                               num_stages=3

flood/flood/utils/batch.py
@@ -11,24 +11,10 @@ import itertools
 import numpy as np
 import torch
 import torch.distributed as dist
-from flood.ops.draft import retrieve_draft_table
 
 from flood.utils.sampling import Sampling
 
-"""
-    meta.batch_size = bs
-    meta.q_offsets = q_offsets
-    meta.k_offsets = k_offsets
-    meta.q_lengths = q_lengths
-    meta.k_lengths = k_lengths
-    meta.k_segs = k_segs
-    meta.max_seqlen_q = max_q_length
-    meta.max_seqlen_k = max_k_length
-    meta.max_seg = max_seg
-    meta.mask = mask
-    meta.qls = qls
-    meta.kls = klss
-"""
+
 
 """
 state=0: undefined
@@ -156,6 +142,9 @@ class Batch:
         self.retrieve_count = retrieve_count
         self.embeddings = embeddings
         self.emb_idx_list = emb_idx_list
+
+        self.cache_src_indices = None 
+        self.cache_dst_indices = None
 
     @staticmethod
     def prefill_batching(reqs,
@@ -427,71 +416,71 @@ class Batch:
                            retrieve_count=4):
 
         bs = len(reqs)
+        max_seg = max([len(x.segs) for x in reqs])
+        l = retrieve_count * spec.branch_length
 
         retrieve_key_ids = [x.output_ids[-2:] if len(x.output_ids) >= 2 else [0,
                                                                               x.output_ids[
                                                                                   0]]
                             for x in reqs]
-        input_ids, masks = retrieve_draft_table(retrieve_key_ids,
-                                         spec.freq_table,
-                                         spec.draft_table,
-                                         spec.table_size,
-                                         spec.branch_length,
-                                         spec.branch_count,
-                                         retrieve_count)
+        input_ids, masks = spec.proposal_draft(retrieve_key_ids, retrieve_count=retrieve_count)
         input_ids = input_ids.view(-1)
 
-        l = retrieve_count * spec.branch_length
-        # max_seg = max([len(x.segs) for x in reqs])
         samplings = []
-        qls = [l] * bs
-
-        max_seg = max([len(x.segs) for x in reqs])
-        position_ids = []
-        draft_offsets = [0]  # used for rope
+        qls = [l] * bs 
+        position_ids = []  # a position id for a seg
+        draft_offsets = [0]  # act as q_offsets in rope
         cache_indices = []
         k_lengths = []
         k_offsets = []
+        k_segs = []
         for i, req in enumerate(reqs):
             s = req.input_length + len(req.output_ids)
             n_seg = len(req.segs)
+            k_segs.append(n_seg)
             offset = req.segs[-1][0]
-            position_ids.extend([s] + [s+1]*(retrieve_count-1))
-            draft_offsets.extend([i*l+(j+1)*spec.branch_length+(0 if j==retrieve_count-1 else 1) for j in range(retrieve_count)])
+            position_ids.extend([s-1] + [s]*(retrieve_count-1))
+            for j in range(retrieve_count):
+                if retrieve_count > 1:
+                    if j == 0:
+                        bl = spec.branch_length + 1
+                    elif j == retrieve_count-1:
+                        bl = spec.branch_length - 1
+                    else:
+                        bl = spec.branch_length
+                else:
+                    bl = spec.branch_length
+                draft_offsets.append(draft_offsets[-1] + bl)
+            cache_indices.extend([offset + s - 1 + j for j in range(l)])
             if max_seg == 1:
                 k_offsets.append(offset)
-                k_lengths.append(s + l - 1)
-                cache_indices.extend([s - 1 + j + offset for j in
-                                               range(
-                                                   l)])
+                k_lengths.append(s + l - 1)  # NOTE: spec with additional `l - 1` tokens
+                if i == bs - 1:
+                    k_offsets.append(offset + s + l - 1)
             else:
                 k_offsets.extend([x[0] for x in req.segs]+[0]*(max_seg-n_seg))
-                cache_indices.extend(
-                    [s - 1 + j + offset[n_seg - 1] for j in
-                     range(l)])
-                k_length = [0] + [x[1] - x[0] for x in req.segs[:-1]] + [
-                    s + l - 1]
-                k_length = k_length + [0] * (max_seg - len(req.segs))
-                k_length = itertools.accumulate(k_length)
+                pre_lengths = [x[1] - x[0] for x in req.segs[:-1]]
+                pre_length = sum(pre_lengths)
+                cur_length = s + l - 1 - pre_length
+                k_length = pre_lengths + [cur_length] + \
+                     [0] * (max_seg - len(req.segs)) + [s + l - 1]
                 k_lengths.extend(k_length)
-        k_offsets.append(reqs[-1].segs[-1][0]+reqs[-1].input_length + len(reqs[-1].output_ids)+l-1)
 
         max_q_length = l
         max_k_length = max(k_lengths)  # NOTE: CHANGED AND NOT VERIFY 
 
+        kls = k_lengths
+        q_offsets = l * torch.arange(bs + 1, dtype=torch.int32, device=device)
+        k_offsets = torch.tensor(k_offsets, dtype=torch.int32, device=device)
+        q_lengths = torch.tensor(qls, dtype=torch.int32, device=device)
+        k_lengths = torch.tensor(k_lengths, dtype=torch.int32, device=device)
+        k_segs = torch.tensor(k_segs, dtype=torch.int32, device=device)
+        cache_indices = torch.tensor(cache_indices,
+                                              dtype=torch.int32, device=device)
         draft_offsets = torch.tensor(draft_offsets, dtype=torch.int32,
                                     device=device)
         position_ids = torch.tensor(position_ids, dtype=torch.int32,
                                     device=device)
-
-        kls = k_lengths
-        q_lengths = qls
-        q_offsets = l * torch.arange(bs + 1, dtype=torch.int32, device=device)
-        k_offsets = torch.tensor(k_offsets, dtype=torch.int32, device=device)
-        q_lengths = torch.tensor(q_lengths, dtype=torch.int32, device=device)
-        k_lengths = torch.tensor(k_lengths, dtype=torch.int32, device=device)
-        cache_indices = torch.tensor(cache_indices,
-                                              dtype=torch.int32, device=device)
 
         for _, req in enumerate(reqs):
             if req.target_ids or req.temperature or req.top_k or req.top_p or req.min_p:
@@ -518,9 +507,10 @@ class Batch:
                      q_lengths=q_lengths,
                      k_lengths=k_lengths,
                      cache_indices=cache_indices,
+                     k_segs=k_segs,
+                     max_seg=max_seg,
                      samplings=samplings,
                      reqs=reqs,
-                     max_seg=max_seg,
                      mask=masks,
                      qls=qls,
                      kls=kls,