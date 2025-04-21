fix lookahead bug

flood/benchmark/ops/bench_seg_attn.py
@@ -29,6 +29,7 @@ def bench_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
     mask_size = 16
     torch_mask = None
     if mode == 'prefill':
+        bs = 1
         if max_seg == 1:
             if even:
                 qls = [1024]
@@ -44,28 +45,30 @@ def bench_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
                 qls = [1025]
                 kls = [1025 * max_seg]
     elif mode == 'decode':
-        qls = [1] * 128
+        bs = 128
+        qls = [1] * bs
         if even:
-            kls = [1024] * 128
+            kls = [1024] * bs
         else:
-            kls = [1025] * 128
+            kls = [1025] * bs
     elif mode == 'mix':
-        tbs = 40
-        qls = [(1024 - tbs +1)//max_seg] + [1] * (tbs-1)
+        bs = 40
+        qls = [(1024 - bs +1)//max_seg] + [1] * (bs-1)
         if even:
-            kls = [1024] * tbs
+            kls = [1024] * bs
         else:
-            kls = [1025] * tbs
+            kls = [1025] * bs
     elif mode == 'spec':
-        qls = [mask_size] * 1
+        bs = 16
+        qls = [mask_size] * bs
         if even:
-            kls = [1024] * 1
+            kls = [1024] * bs
         else:
-            kls = [1025] * 1
+            kls = [1025] * bs
 
-        assert qls[0] == mask_size and len(qls) == 1 
+        assert all([x==mask_size for x in qls])
 
-        masks = torch.tril(torch.ones((mask_size, mask_size), 
+        masks = torch.tril(torch.ones(( mask_size, mask_size), 
                                 dtype=torch.int8,
                                 device=device), 0)
         for i in range(mask_size):
@@ -109,7 +112,6 @@ def bench_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
     k = torch.cat(ks, 0)
     v = torch.cat(vs, 0)
 
-    desc = f'seg:{max_seg} mode:{mode} bs:{len(qls)} q:{qls[0]} k:{kls[0]} qo_head:{qo_head} kv_head:{kv_head} dim:{dim}'
 
     # ks = torch.stack([k]*group,2).view(sum(kls), qo_head, dim)
     # vs = torch.stack([v]*group,2).view(sum(kls), qo_head, dim)
@@ -121,7 +123,11 @@ def bench_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
     print(f'\nseg:{max_seg} mode:{mode} online:{online_scale} bs:{len(qls)} q:{qls[0]} k:{kls[0]} qo_head:{qo_head} kv_head:{kv_head} dim:{dim}')
     n_repeat = 1000
     if mode == 'spec':
-        org_time = benchmark_func(torch_attn, q[None], k[None], v[None], causal=True, mask=torch_mask,
+        org_time = benchmark_func(torch_attn, 
+                                  q.view(bs, mask_size, qo_head, dim), 
+                                  k.view(bs, kls[0], kv_head, dim), 
+                                  v.view(bs, kls[0], kv_head, dim), 
+                                  causal=True, mask=torch_mask,
                                 ref_flops=flops,
                                 n_repeat=n_repeat)
     else:
@@ -133,11 +139,11 @@ def bench_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
                 n_repeat=n_repeat, ref_time=org_time, ref_flops=flops)
 
 if __name__ == '__main__':
-    for max_seg in [1,2,4]:
-        for mode in ['prefill', 'decode', 'mix', 'spec']:
-            for even in [True, False]:
-                for online_scale in [False, True]:
-                    bench_seg_attn(max_seg=max_seg, mode=mode, even=even, online_scale=online_scale)
+    # for max_seg in [1,2,4]:
+    #     for mode in ['prefill', 'decode', 'mix', 'spec']:
+    #         for even in [True, False]:
+    #             for online_scale in [False, True]:
+    #                 bench_seg_attn(max_seg=max_seg, mode=mode, even=even, online_scale=online_scale)
 
     # bench_seg_attn(max_seg=1, mode='decode', even=True, online_scale=False)
-    # bench_seg_attn(max_seg=2, mode='spec', even=True, online_scale=False)
+    bench_seg_attn(max_seg=1, mode='spec', even=True, online_scale=False)

flood/example/lookahead_example.py
@@ -30,8 +30,9 @@ if __name__ == '__main__':
     # model_path = '/mntnlp/nanxiao/deepseekv3'
     # model_path = '/agent/nanxiao/models/Qwen2.5-32B-Instruct'
 
-    prompts = ['介绍一下杭州',
-    '鲁迅有哪些作品？',
+    prompts = [
+    '介绍一下杭州',
+    # '鲁迅有哪些作品？',
     # 'Consider a sequence of real numbers \( a_1, a_2, a_3, \ldots \) defined as follows: \( a_1 = 1 \)\n For \( n \geq 1 \), \( a_{n+1} = \\frac{a_n + 2}{a_n + 1} \).\nDetermine the value of \( a_{2024} \)'
     ]
 
@@ -53,30 +54,40 @@ if __name__ == '__main__':
             )
             reqs.append(Request(i, input_text=text, output_length=512))
 
+    # with profile(activities=[ProfilerActivity.CUDA, ProfilerActivity.CPU, ProfilerActivity.XPU], record_shapes=False, profile_memory=False, with_flops=True, with_stack=True, with_modules=True) as prof:
     if True:
         worker = LLM(model_path,
                     n_stage=1,  # gpus
                     n_proc=1,
                     chunk_size=1024,
+                    #  model_dtype=torch.float8_e4m3fn,
                     cache_size=None,
                     slot_fully_alloc_under=1024,
                     tune_alloc_size=False,
                     eos_token_id=None,
-                    debug=True,
+                    debug=False,
                     kernels=('sa',),
+                    spec_algo = 'lookahead',
+                    spec_branch_length=8,
+                    max_spec_branch_count=8,
                     logger='example.log')
-
 
         # start process
         input_queue, chunk_queue, working_queue, output_queues = worker.initialize()
         worker.launch(input_queue, chunk_queue, working_queue, output_queues)
 
         # do benchmark
-        for i, req in enumerate(worker.request_stream_generate(reqs,
-                                                    input_queue,
-                                                    output_queues,
-                                                    print_count=0)):
-            print('\n\n')
-            print(f'prompt-{i}: ', req.input_text)
-            print(f'answer-{i}: ', req.output_text)
-        time.sleep(1.0)
+        for _ in range(10):
+            for i, req in enumerate(worker.request_stream_generate(reqs,
+                                                        input_queue,
+                                                        output_queues,
+                                                        print_count=0)):
+                print('\n\n')
+                print(f'prompt-{i}: ', req.input_text)
+                print(f'answer-{i}: ', req.output_text)
+            time.sleep(1.0)
+
+    # print(prof.key_averages().table(sort_by=None, top_level_events_only=True, row_limit=2000))
+    # print(prof.key_averages(group_by_stack_n=5).table(sort_by=None, row_limit=100))
+    # prof.export_chrome_trace("trace.json")
+

flood/test/test_seg_attn.py
@@ -25,13 +25,14 @@ def seg_attn(q, k, v, meta, online_scale=True):
 def test_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
     device = torch.device('cuda:0')
     dtype = torch.bfloat16
-    qo_head = 32
+    qo_head = 64
     kv_head = 8
     dim = 128
     masks = None
     mask_size = 16
     torch_mask = None
     if mode == 'prefill':
+        bs = 1
         if max_seg == 1:
             if even:
                 qls = [1024]
@@ -47,40 +48,43 @@ def test_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
                 qls = [1025]
                 kls = [1025 * max_seg]
     elif mode == 'decode':
-        qls = [1] * 128
+        bs = 128
+        qls = [1] * bs
         if even:
-            kls = [1024] * 128
+            kls = [1024] * bs
         else:
-            kls = [1025] * 128
+            kls = [1025] * bs
     elif mode == 'mix':
-        tbs = 40
-        qls = [(1024 - tbs +1)//max_seg] + [1] * (tbs-1)
+        bs = 40
+        qls = [(1024 - bs +1)//max_seg] + [1] * (bs-1)
         if even:
-            kls = [1024] * tbs
+            kls = [1024] * bs
         else:
-            kls = [1025] * tbs
+            kls = [1025] * bs
     elif mode == 'spec':
-        qls = [mask_size] * 1
+        bs = 16
+        qls = [mask_size] * bs
         if even:
-            kls = [1024] * 1
+            kls = [1024] * bs
         else:
-            kls = [1025] * 1
+            kls = [1025] * bs
 
-        assert qls[0] == mask_size and len(qls) == 1 
+        assert all([x==mask_size for x in qls])
 
-        masks = torch.tril(torch.ones((mask_size, mask_size), 
+        masks = torch.tril(torch.ones((bs, mask_size, mask_size), 
                                 dtype=torch.int8,
                                 device=device), 0)
         for i in range(mask_size):
             for j in range(mask_size):
                 if j < i:
-                    masks[i, j] = random.randint(0, 1)
+                    masks[:, i, j] = random.randint(0, 1)
         # print(f'{masks}')
 
-        full_mask = torch.ones((qls[0], kls[0] - qls[0]),
+        full_mask = torch.ones((bs, qls[0], kls[0] - qls[0]),
                                 dtype=torch.float32,
                                 device=device)
-        torch_mask = -10000 * (1 - torch.cat([full_mask,masks.float()], dim=1))
+        torch_mask = -10000 * (1 - torch.cat([full_mask,masks.float()], dim=2))
+        torch_mask = torch_mask[:,None]
     else:
         raise ValueError(f'unknown mode:{mode}')
 
@@ -122,14 +126,22 @@ def test_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
     flash_attn_meta = get_flash_attn_meta(qls, kls)
 
     if mode == 'spec':
-        org_output = torch_attn(q[None], k[None], v[None], causal=True, mask=torch_mask)[0]
+        org_output = torch_attn(q.view(bs, mask_size, qo_head, dim), 
+                                k.view(bs, kls[0], kv_head, dim), 
+                                v.view(bs, kls[0], kv_head, dim),
+                                causal=True, 
+                                mask=torch_mask).view(bs*qls[0], qo_head, dim)
     else:
         org_output = flash_attn(q, k, v, flash_attn_meta)
+
 
     torch.cuda.synchronize()
 
     opt_output = seg_attn(q, k, v, seg_attn_meta, online_scale=online_scale)
     torch.cuda.synchronize()
+
+    print(org_output.shape, opt_output.shape)
+
 
     # print("org",org_output.dtype,org_output.shape)
     # print("opt",opt_output.dtype,opt_output.shape)
@@ -159,10 +171,10 @@ def test_seg_attn(max_seg=1, mode='prefill', even=True, online_scale=True):
                                    rtol=0.05, atol=0.1)
 
 if __name__ == '__main__':
-    for max_seg in [1,2,4]:
-        for mode in ['prefill', 'decode', 'mix','spec']:
-            for even in [True, False]:
-                for online_scale in [True, False]:
-                    test_seg_attn(max_seg=max_seg, mode=mode, even=even, online_scale=online_scale)
+    # for max_seg in [1,2,4]:
+    #     for mode in ['prefill', 'decode', 'mix','spec']:
+    #         for even in [True, False]:
+    #             for online_scale in [True, False]:
+    #                 test_seg_attn(max_seg=max_seg, mode=mode, even=even, online_scale=online_scale)
 
-    # test_seg_attn(max_seg=2, mode='spec', even=False, online_scale=True)
+    test_seg_attn(max_seg=2, mode='spec', even=False, online_scale=True)