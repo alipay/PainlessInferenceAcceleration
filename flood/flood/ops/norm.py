2f59278aa9ff3dcbc7b451f73c2da7bcbe7449ef
hujingyu.hjy@antgroup.com <hujingyu.hjy@antgroup.com>
2025-05-16 14:23:48 +0800

    replace rearrage

--- flood/flood/ops/norm.py
+++ flood/flood/ops/norm.py
@@ -5,7 +5,6 @@ Copyright (c) Ant Financial Service Group and its affiliates.
 
 import torch
 import flood_cuda
-from einops import rearrange
 
 class RMSNorm(torch.nn.Module):
 
@@ -30,15 +29,17 @@ class RMSGroupNorm(torch.nn.Module):
         super().__init__()
         self.weight = torch.nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = eps
+        self.hidden_size = hidden_size
         self.linear_attn_norm_group_size = linear_attn_norm_group_size
+        self.per_group_hidden_size = hidden_size // linear_attn_norm_group_size
 
     def forward(self,x: torch.Tensor,) -> torch.Tensor:
-        x = rearrange(x, 'bt (g h) d -> bt g (h d)', g=self.linear_attn_norm_group_size)
+        x = x.view(-1, self.linear_attn_norm_group_size, self.per_group_hidden_size)
         orig_dtype = x.dtype
         x = x.to(torch.float32)
         variance = x.pow(2).mean(dim=-1, keepdim=True, dtype=torch.float32)
         x = x * torch.rsqrt(variance + self.variance_epsilon)
         x = x.to(orig_dtype)
-        x = rearrange(x, 'bt g d -> bt (g d)')
+        x = x.view(-1, self.hidden_size)
         return x * self.weight
     
