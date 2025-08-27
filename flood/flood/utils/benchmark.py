4265bbe377e4393b274b20c7d39d3bb331318549
hujingyu.hjy <hujingyu.hjy@antgroup.com>
2025-08-27 19:33:15 +0800

    fused qknorm rope kernel

--- flood/flood/utils/benchmark.py
+++ flood/flood/utils/benchmark.py
@@ -9,6 +9,8 @@ import numpy as np
 import math
 
 import torch
+from torch.profiler import profile, ProfilerActivity
+
 try:
     import flash_attn_2_cuda
 except:
@@ -20,33 +22,47 @@ except:
 
 
 
-
 def seed_everything(seed: int) -> None:
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
-
-
-def benchmark_func(fn, *args, n_warmup=100, n_repeat=1000, ref_time=None,
-                   ref_flops=None, desc='', **kwargs):
-    func_name = fn.__name__
+def benchmark_func(fn, *args, n_warmup=10, n_repeat=100, ref_flops=None,
+                   ref_bytes=None, ref_time=None, 
+                   n_profile=0, trace_dir=None,
+                   name='', **kwargs):
+    func_name = getattr(fn, '__name__', None)
+    func_name = name if func_name == 'apply' or func_name is None else func_name
 
     for i in range(n_warmup):
         fn(*args, **kwargs)
 
     start_events = [torch.cuda.Event(enable_timing=True) for _ in
                     range(n_repeat)]
-    end_events = [torch.cuda.Event(enable_timing=True) for _ in 
-                    range(n_repeat)]
+    end_events = [torch.cuda.Event(enable_timing=True) for _ in range(n_repeat)]
 
+    ts = time.time()
     for i in range(n_repeat):
         start_events[i].record()
         fn(*args, **kwargs)
         end_events[i].record()
 
     torch.cuda.synchronize()
+
+    if n_profile > 0:
+        with profile(activities=[ProfilerActivity.CPU, 
+                                 ProfilerActivity.CUDA,
+                                 ProfilerActivity.XPU]) as prof:
+            for i in range(n_profile):
+                fn(*args, **kwargs)
+        print(prof.key_averages().table(sort_by="cuda_time_total", 
+                                        top_level_events_only=True,
+                                        row_limit=100))
+        if trace_dir is not None:
+            assert trace_dir.endswith('.json')
+            prof.export_chrome_trace(trace_dir)
+
 
     times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
     times = sorted(times)
@@ -55,18 +71,19 @@ def benchmark_func(fn, *args, n_warmup=100, n_repeat=1000, ref_time=None,
 
     average_event_time = times * 1000 / (n_repeat - 2 * clip)
 
-    line = f'{func_name:<32} {desc} time:{average_event_time:.1f} us'
-
+    fs = ''
     if ref_flops is not None:
-        perf = ref_flops / 1e12 / (
-                average_event_time / 1e6)
-        line += f' perf:{perf:.2f} TFLOPS'
-
+        flops = ref_flops / 1e12 / (average_event_time / 1e6)
+        fs = f'FLOPS:{flops:.2f}T'
+    bs = ''
+    if ref_bytes is not None:
+        bs = f'bandwidth:{ref_bytes / average_event_time / 1e3:.1f}G/S'
+    ss = ''
     if ref_time is not None:
-        speedup = ref_time / average_event_time 
-        line += f' speedup:{speedup:.2f}'
-    
-    print(line)
+        ss = f'speedup:{ref_time / average_event_time:.3f}'
+
+    print(
+        f'{func_name:<30} {name} time:{average_event_time:.1f} us {fs} {bs} {ss}')
     return average_event_time
 
 
